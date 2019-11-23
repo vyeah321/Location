1c5bb12e91c82d4b60e1f4b858a7eadfcfa64677
vyeah321 <vyeah321@gmail.com>
2019-11-23 12:03:55 +0900

    Initial Commit

---
+++ Location/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Location
+//
+//  Created by TSUBUSAKI AKIHIRO on 2019/11/23.
+//  Copyright © 2019 TSUBUSAKI AKIHIRO. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
